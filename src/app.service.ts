084465f69bd2eec63d4c27a32f8fa24e1d8521ee
Yutao DU 杜玉涛 <yutao.du@nio.com>
2022-05-11 22:55:05 +0800

    更改了返回的数据值 lastValueFrom

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,7 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { AxiosResponse } from 'axios';
-import { map, Observable } from 'rxjs';
+import { lastValueFrom, map, Observable } from 'rxjs';
 
 @Injectable()
 export class AppService {
@@ -11,14 +11,23 @@ export class AppService {
     return 'Hello World!';
   }
 
-  getAccessToken(postData: {
+  async getAccessToken(postData: {
     grant_type: string;
     code: string;
     app_id: string;
     app_secret: string;
-  }): Observable<AxiosResponse<Object>> {
-    return this.httpService
-      .post('https://open.feishu.cn/open-apis/authen/v1/access_token', postData)
-      .pipe(map((response) => response.data));
+  }): Promise<Object> {
+    const data = await lastValueFrom(
+      this.httpService
+        .post(
+          'https://open.feishu.cn/open-apis/authen/v1/access_token',
+          postData,
+        )
+        .pipe(map((response) => response.data)),
+    );
+
+    console.log(data);
+
+    return data.data;
   }
 }
